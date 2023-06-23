901e05893dc9757feb6bb1d9a9864dcb7d065285
Steve <sjd734312@gmail.com>
2023-06-23 09:15:32 -0400

    post

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Add your code here
+// function submitData(){
+//    return fetch("http://localhost:3000/users", {
+//  method: "POST",
+//  headers: {
+//    "Content-Type": "application/json",
+//    "Accept": "application/json",
+//  },
+//  body: JSON.stringify({
+//    name: "Steve",
+ //   email: "steve@steve.com",
+//  }),
+//});
+//}
+
+
+
+const configObj = {
+    method: "POST",
+    headers: {
+       "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+         name: "Steve",
+         email: "steve@steve.com",
+  }),
+}
+
+function submitData(){   
+    return fetch("http://localhost:3000/users", configObj)
+      .then((response) => {
+        return response.json();
+      })
+      .then((object) => {
+        const newID = object.id;
+        
+        const element = document.createElement("div")
+        element.textContent = newID;
+        document.body.appendChild(element);
+
+        return object
+      })
+
+      .catch((error) => {
+        const element = document.createElement("div")
+        element.textContent = error.message
+        document.body.appendChild(element)
+      });
+    }
+
+
+   
+
